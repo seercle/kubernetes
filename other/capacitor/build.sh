e02fd6d82858e5716e1b86278f31089f1fa9c531
seercle <axel.vivenot@outlook.fr>
2025-08-08 16:58:58 +0200

    added capacitor & fix gitlab

---
+++ other/capacitor/build.sh
@@ -0,0 +1,9 @@
+# Retrieve the latest version
+curl -L "https://github.com/gimlet-io/capacitor/releases/download/capacitor-next/next-$(uname)-$(uname -m)" -o next
+chmod +x next
+
+docker build -t harbor.seercle.com/homelab/capacitor:latest .
+
+docker login harbor.seercle.com
+
+docker push harbor.seercle.com/homelab/capacitor:latest
