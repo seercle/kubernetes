67625809a1a2d1ca13926fbba9a0c22497714add
seercle <axel.vivenot@outlook.fr>
2025-08-08 17:04:46 +0200

    now pulling kubeconfig

--- other/capacitor/build.sh
+++ other/capacitor/build.sh
@@ -2,8 +2,16 @@
 curl -L "https://github.com/gimlet-io/capacitor/releases/download/capacitor-next/next-$(uname)-$(uname -m)" -o next
 chmod +x next
 
+# Retrieve kube config
+cp ~/.kube/config kubeconfig
+
 docker build -t harbor.seercle.com/homelab/capacitor:latest .
 
 docker login harbor.seercle.com
 
 docker push harbor.seercle.com/homelab/capacitor:latest
+
+# Cleanup
+rm next
+rm kubeconfig
+docker rmi harbor.seercle.com/homelab/capacitor:latest
