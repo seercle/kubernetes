d2f955310b1c51e7bd4b0978ff23a85ae19b3117
seercle <notseercle@gmail.com>
2025-10-16 00:13:55 +0200

    fixed blocky, harbor, removed some services, updated capacitor

--- other/capacitor/build.sh
+++ other/capacitor/build.sh
@@ -1,16 +1,13 @@
-# Retrieve the latest version
-curl -L "https://github.com/gimlet-io/capacitor/releases/download/capacitor-next/next-$(uname)-$(uname -m)" -o next
-chmod +x next
+wget -qO- https://gimlet.io/install-capacitor | bash
 
+cp ~/.local/bin/next .
 sops --decrypt config > kubeconfig
 
 docker build -t harbor.seercle.com/homelab/capacitor:latest .
 
 docker login harbor.seercle.com
-
 docker push harbor.seercle.com/homelab/capacitor:latest
 
-# Cleanup
 rm next
 rm kubeconfig
 docker rmi harbor.seercle.com/homelab/capacitor:latest
